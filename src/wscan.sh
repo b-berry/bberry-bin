700b9fa5d67cd8d1ce17ec8d3283cc2440ad26c4
Bryan Berry <berr1167@gmail.com>
2024-12-30 13:07:02 -0800

    src/wscan: Add positional parameters

--- src/wscan.sh
+++ src/wscan.sh
@@ -9,7 +9,10 @@ if [ $# -lt 1 ]; then
   nmcli dev wifi
 elif [ $# -eq 2 ]; then
   # Connect for first time
-  nmcli dev wifi con "$1" password caffeine name "$2"
+  nmcli dev wifi con "$1" password "$2"
+elif [ $# -eq 3 ]; then
+  # Connect for first time
+  nmcli dev wifi con "$1" password "$2" name "$3"
 elif [ $# -eq 1 ]; then
   # Connect to existing profile
   nmcli con up id "$1"
