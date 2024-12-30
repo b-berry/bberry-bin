9ece37dc132b1c5498ca2b70ada7335241ee3a53
Bryan Berry <berr1167@gmail.com>
2024-12-30 13:12:37 -0800

    chore: dotfiles/.config/i3: update to latest

---
+++ dotfiles/.config/i3/scripts/i3exit.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+WALLPAPERDIR="/home/bberry/src/i3wm-nord/wallpapers"
+
+case "$1" in
+    lock)
+        i3lock -i ${WALLPAPERDIR}/moon-nord.png
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        i3lock -i ${WALLPAPERDIR}/moon-nord.png && systemctl suspend
+        ;;
+    hibernate)
+        systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        exit 2
+esac
+
+exit 0
