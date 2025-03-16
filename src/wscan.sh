dcd210ac8529ce48ba798057e989887d10d53c58
Bryan Berry <berr1167@gmail.com>
2025-03-16 09:02:54 -0700

    src/wscan: Add disconnect option

--- src/wscan.sh
+++ src/wscan.sh
@@ -6,14 +6,18 @@
 
 if [ $# -lt 1 ]; then
   # List Wifi networks
-  nmcli dev wifi
+  nmcli -p dev wifi
 elif [ $# -eq 2 ]; then
   # Connect for first time
   nmcli dev wifi con "$1" password "$2"
 elif [ $# -eq 3 ]; then
-  # Connect for first time
+  # Connect for first time and save network
   nmcli dev wifi con "$1" password "$2" name "$3"
 elif [ $# -eq 1 ]; then
-  # Connect to existing profile
-  nmcli con up id "$1"
+  if [ $1 == "disconnect" ]; then
+    nmcli dev disconnect wlp3s0
+  else
+    # Connect to existing profile OR connect to passwordless network
+    nmcli con up id "$1" || nmcli dev wifi con "$1"
+  fi
 fi
